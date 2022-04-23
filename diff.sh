a3278edb88f8708751c4b41dc911ebd6109aa1a5
Zachary Huff <zach.huff.386@gmail.com>
2022-04-23 17:25:51 -0400

    Add diff

---
+++ diff.sh
@@ -0,0 +1,4 @@
+rsync --human-readable --archive --xattrs --progress --super --dry-run --delete \
+  --exclude /.git \
+  --exclude /mirror \
+  /home/cloud/git/pritunl-pacur/ /home/cloud/git/pritunl-pacur-stable/
