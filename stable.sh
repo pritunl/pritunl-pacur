96bf9676585e3513b3ba662a45a25b1dc681ebb2
Zachary Huff <zach.huff.386@gmail.com>
2022-04-23 17:24:11 -0400

    Update stable

--- stable.sh
+++ stable.sh
@@ -1,1 +1,6 @@
+if [ ! -n "$1" ]; then
+  echo "target unset"
+  exit 1
+fi
+
 sudo rsync --human-readable --archive --xattrs --progress --delete --super /home/cloud/git/pritunl-pacur/$1/ /home/cloud/git/pritunl-pacur-stable/$1/
