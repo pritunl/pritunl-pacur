ba463dc4a8b5c9bbb730e0d9d0db074557faa68e
Zachary Huff <zach.huff.386@gmail.com>
2024-07-26 16:14:10 -0400

    Add warning to upload stable

--- upload-stable.sh
+++ upload-stable.sh
@@ -1,2 +1,7 @@
+read -p "UPLOAD STABLE (y/n)? " CONT
+if [ "$CONT" != "y" ]; then
+  exit;
+fi
+
 mc mirror --remove --overwrite --md5 mirror repo-east/stable
 mc mirror --remove --overwrite --md5 mirror repo-west/stable
