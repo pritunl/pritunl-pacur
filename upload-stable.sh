e013a50cad33c9922c6e86fb635effe110cbfdfe
Zachary Huff <zach.huff.386@gmail.com>
2024-10-16 06:19:22 -0400

    Update upload scripts

--- upload-stable.sh
+++ upload-stable.sh
@@ -3,5 +3,5 @@ if [ "$CONT" != "y" ]; then
   exit;
 fi
 
-mc mirror --remove --overwrite --md5 mirror repo-east/stable
-mc mirror --remove --overwrite --md5 mirror repo-west/stable
+sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur-stable/mirror:/mirror:Z localhost/tools mirror --remove --overwrite --md5 /mirror repo-east/stable
+sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur-stable/mirror:/mirror:Z localhost/tools mirror --remove --overwrite --md5 /mirror repo-west/stable
--- upload-unstable.sh
+++ upload-unstable.sh
@@ -1,2 +1,2 @@
-mc mirror --remove --overwrite --md5 mirror repo-east/unstable
-mc mirror --remove --overwrite --md5 mirror repo-west/unstable
+sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --remove --overwrite --md5 /mirror repo-east/unstable
+sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --remove --overwrite --md5 /mirror repo-west/unstable
