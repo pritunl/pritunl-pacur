66d888cd65ba1db1a8f68417015f1d18593473e2
Zachary Huff <zach.huff.386@gmail.com>
2024-10-23 11:53:56 -0400

    Add minio client

--- tools/Readme.md
+++ tools/Readme.md
@@ -1,3 +1,2 @@
 sudo podman build --rm -t tools .
-sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z localhost/tools ls repo-east
-sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z localhost/tools ls repo-west
+sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z localhost/tools ls pritunl-repo
--- upload-stable.sh
+++ upload-stable.sh
@@ -1,2 +1,2 @@
 sudo python3 autoindex.py stable
-sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur-stable/mirror:/mirror:Z localhost/tools mirror --remove --overwrite --md5 /mirror r2/pritunl-repo/stable
+sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur-stable/mirror:/mirror:Z localhost/tools mirror --summary --remove --overwrite --md5 --retry --checksum=MD5 /mirror r2/pritunl-repo/stable
