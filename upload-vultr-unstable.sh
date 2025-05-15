74e3d0e5e53a3287bcea2410e62323350e442950
Zachary Huff <zach.huff.386@gmail.com>
2025-05-15 05:26:17 -0400

    Update vultr scripts

--- upload-vultr-unstable.sh
+++ upload-vultr-unstable.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo python3 autoindex.py stable
-sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --summary --overwrite --md5 --retry --checksum=MD5 --disable-multipart /mirror vultr-east/repo.pritunl.com/unstable
+sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --summary --remove --overwrite --md5 --retry --checksum=MD5 --disable-multipart /mirror vultr-east/repo.pritunl.com/unstable
