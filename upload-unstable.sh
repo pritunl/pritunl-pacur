1a84c93999a30f42f9ab1beda5cddc7a9652dac0
Zachary Huff <zach.huff.386@gmail.com>
2025-05-30 07:28:49 -0400

    Update build scripts

--- upload-unstable.sh
+++ upload-unstable.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo python3 autoindex.py unstable
 sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --summary --remove --overwrite --md5 --retry --checksum=MD5 --disable-multipart /mirror vultr-east/repo.pritunl.com/unstable
 sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --summary --remove --overwrite --md5 --retry --checksum=MD5 --disable-multipart /mirror r2/pritunl-repo-east/unstable
 sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --summary --remove --overwrite --md5 --retry --checksum=MD5 --disable-multipart /mirror r2/pritunl-repo-west/unstable
