0135bbb8e157d660aade8f5acc7d727252776962
Zachary Huff <zach.huff.386@gmail.com>
2026-01-29 08:08:18 -0500

    Update upload unstable

--- upload-unstable.sh
+++ upload-unstable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo python3 autoindex.py unstable
-sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --summary --remove --overwrite --md5 --retry --checksum=MD5 --disable-multipart /mirror vultr-east/repo.pritunl.com/unstable
-sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --summary --remove --overwrite --md5 --retry --checksum=MD5 --disable-multipart /mirror r2/pritunl-repo-east/unstable
-sudo podman run --rm -v /home/cloud/.mc:/root/.mc:Z -v /home/cloud/git/pritunl-pacur/mirror:/mirror:Z localhost/tools mirror --summary --remove --overwrite --md5 --retry --checksum=MD5 --disable-multipart /mirror r2/pritunl-repo-west/unstable
+sudo python3 autoindex.py stable
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ r2-east:/unstable/
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ r2-west:/unstable/
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ vultr-east:/unstable/
