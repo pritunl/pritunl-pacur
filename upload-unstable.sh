30aafb398322862e8d3d062de047bdf788a5399e
Zachary Huff <zach.huff.386@gmail.com>
2026-03-24 08:19:14 -0400

    Fix label in upload unstable

--- upload-unstable.sh
+++ upload-unstable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo python3 autoindex.py stable
+sudo python3 autoindex.py unstable
 s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ r2-east:/unstable/
 s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ r2-west:/unstable/
 s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ vultr-east:/unstable/
