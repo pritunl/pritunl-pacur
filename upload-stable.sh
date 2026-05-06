cc6a48898e2c6a7227661ed72aa4cf8444f3e073
Zachary Huff <zach.huff.386@gmail.com>
2026-05-06 19:09:28 -0400

    Update upload scripts

--- upload-stable.sh
+++ upload-stable.sh
@@ -2,4 +2,4 @@
 sudo python3 autoindex.py stable
 s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/ r2-east:/stable/
 s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/ r2-west:/stable/
-s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/ vultr-east:/stable/
+#s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/ vultr-east:/stable/
--- upload-unstable.sh
+++ upload-unstable.sh
@@ -2,4 +2,4 @@
 sudo python3 autoindex.py unstable
 s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ r2-east:/unstable/
 s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ r2-west:/unstable/
-s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ vultr-east:/unstable/
+#s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ vultr-east:/unstable/
