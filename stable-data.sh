25c1fefb596aae9aa5a61b519da19571e603177d
Zachary Huff <zach.huff.386@gmail.com>
2026-08-11 16:29:08 -0400

    Update stable data script

--- stable-data.sh
+++ stable-data.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo rsync --human-readable --archive --xattrs --progress --delete --super \
   /home/cloud/git/pritunl-pacur/.git/ /home/cloud/git/pritunl-pacur-stable/.git/
-sudo rsync --human-readable --archive --xattrs --progress --delete --super \
-  /home/cloud/git/pritunl-pacur/tools/ /home/cloud/git/pritunl-pacur-stable/tools/
 sudo rsync --human-readable --archive --xattrs --progress --super \
   --include=".gitignore" --include="/*.sh" --include="/*.json" \
   --include="/*.html" --include="/*.py" --include="/*.key" --exclude="/*/" \
