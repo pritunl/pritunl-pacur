2738205e360c6503e3f7b944d35c1423fbbfcb7a
Zachary Huff <zach.huff.386@gmail.com>
2026-09-09 07:38:28 -0400

    Update upload scripts

--- upload-stable.sh
+++ upload-stable.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo python3 autoindex.py stable
-s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/ r2-east:/stable/
-s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/ r2-west:/stable/
-#s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/ vultr-east:/stable/
+s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/apt/ r2-east:/stable/apt/
+s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/pacman/ r2-east:/stable/pacman/
+s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/yum/ r2-east:/stable/yum/
+s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/apt/ r2-west:/stable/apt/
+s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/pacman/ r2-west:/stable/pacman/
+s3c mirror --remove /home/cloud/git/pritunl-pacur-stable/mirror/yum/ r2-west:/stable/yum/
+s3c index --jobs 30 r2-east:/stable/
+s3c index --jobs 30 r2-west:/stable/
--- upload-unstable.sh
+++ upload-unstable.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo python3 autoindex.py unstable
-s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ r2-east:/unstable/
-s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ r2-west:/unstable/
-#s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/ vultr-east:/unstable/
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/apt/ r2-east:/unstable/apt/
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/pacman/ r2-east:/unstable/pacman/
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/yum/ r2-east:/unstable/yum/
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/apt/ r2-west:/unstable/apt/
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/pacman/ r2-west:/unstable/pacman/
+s3c mirror --remove /home/cloud/git/pritunl-pacur/mirror/yum/ r2-west:/unstable/yum/
+s3c index --jobs 30 r2-east:/unstable/
+s3c index --jobs 30 r2-west:/unstable/
